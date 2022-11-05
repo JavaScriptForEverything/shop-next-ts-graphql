d88051b247caa34087854d25798cd86a4e4e4b1a
Riajul Islam <javascriptforeverything@gmail.com>
2022-11-05 15:05:00 +0600

    add email and password checking in isFormValidator and modify UI by adding login and clear button

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client'
+import isEmail from 'validator/lib/isEmail'
 
 import { MUTATION_LOGIN } from '@/graphql/query/user'
 import { UserDocument } from '@/shared/types/user'
 
-import { TextField } from '@mui/material'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 
 
 const inputItems = [
@@ -26,7 +29,7 @@ type FieldsState = {
 	email: string
 	password: string
 }
-const loginFields: FieldsState = {
+const initialState: FieldsState = {
 	email: '',
 	password: ''
 }
@@ -44,6 +47,9 @@ type TempObj = {
 const isFormValid = (fields: FieldsState, setFieldsError: React.Dispatch<React.SetStateAction<TempObj>>) => {
 	const tempObj: TempObj = {}
 
+	if( !isEmail(fields.email) ) tempObj.email = 'Invalid Email Address'
+	if( fields.password.length < 8 ) tempObj.password = 'password must by atleast 8 character long'
+
 	Object.keys(fields).forEach(field => {
 		if(!fields[field as keyof FieldsState]) tempObj[field] = `'${field}' is emapty`
 	})
@@ -53,13 +59,17 @@ const isFormValid = (fields: FieldsState, setFieldsError: React.Dispatch<React.S
 }
 
 const Login = () => {
-	const [ fields, setFields ] = useState<FieldsState>(loginFields)
-	const [ fieldsError, setFieldsError ] = useState<TempObj>(loginFields)
+	const [ fields, setFields ] = useState<FieldsState>(initialState)
+	const [ fieldsError, setFieldsError ] = useState<TempObj>(initialState)
 
 	const [ loginUser, { data, error } ] = useMutation<LoginMutationState, LoginVariables>(MUTATION_LOGIN)
 
 	const handleChange = (field: string ) => (evt: React.ChangeEvent<HTMLInputElement>) => {
 		setFields({ ...fields, [field]: evt.target.value })
+	}
+	const handleFormReset = () => {
+		setFields(initialState)
+		setFieldsError(initialState)
 	}
 	const handleSubmit = async(evt: React.FormEvent<HTMLFormElement>) => {
 		evt.preventDefault()
@@ -72,7 +82,7 @@ const Login = () => {
 		try {
 			// addUser({ variables: {input: { email: 'abc', password: 'asdf' } } })
 			const result = await loginUser({ variables: { input: fields } })
-			setFields(loginFields)
+			setFields(initialState)
 			// console.log(result)
 		} catch (err) {
 			console.log(err)
@@ -85,77 +95,35 @@ const Login = () => {
 	return (
 		<>
 			<form noValidate onSubmit={handleSubmit} >
-				{inputItems.map(({ name, label, placeholder, type }) => (
+				{inputItems.map(({ name, label, placeholder, type }, index) => (
 					<TextField key={name}
 						label={label}
 						placeholder={placeholder}
 						InputLabelProps={{ shrink: true }}
 						required
 						fullWidth
-						autoFocus
+						autoFocus={index === 0}
 						margin='dense'
 
 						type={type}
 						value={fields[name as keyof FieldsState]} 					// method-1: geting type with type
-						// value={fields[name as keyof typeof fields]} 			// method-2: geting type from value
 						onChange={handleChange(name)}
 
-						// error={!!fieldsError[name]}
-						// error={!!fieldsError[name as keyof FieldsState]}
 						error={!!fieldsError[name as keyof typeof fieldsError]}
 						helperText={fieldsError[name as keyof typeof fieldsError]}
 					/>
 				))}
 
 
-
-				{/* {inputItems.map(({ name, type, label, placeholder }) => (
-					<div key={name}>
-						<div style={{ display: 'flex', gap: 24 }}>
-							<label htmlFor={name}>{label}</label>
-							<div style={{ display: 'flex', flexDirection: 'column' }}>
-								<input 
-									id={name}
-									placeholder={placeholder}
-									type={name}
-									value={fields[name as keyof FieldsState]} 
-							// 	value={fields[name as keyof typeof fields]} 
-									onChange={handleChange(name)} 
-								/>
-								{fieldsError[name as keyof typeof fieldsError] && ( <small style={{ color: 'red' }} >
-									{fieldsError[name as keyof typeof fieldsError]}
-									</small> 
-								)}
-							</div>
-						</div>
-
-					</div>
-				))} */}
-
-
-				{/* <div>
-					<label htmlFor='email'>Your Email :</label>
-					<input 
-						id='email' 
-						placeholder='abc@gmail.com'
-						type='email' 
-						value={fields.email} 
-						onChange={handleChange('email')} 
-					/>
-				</div>
-
-				<div>
-					<label htmlFor='password'> Password : </label>
-					<input 
-						id='password' 
-						placeholder='********'
-						type='password' 
-						value={fields.password} 
-						onChange={handleChange('password')} 
-					/>
-				</div> */}
-
-				<button type='submit'>Login</button>
+				<Box sx={{
+					display: 'flex',
+					gap: 1,
+					justifyContent: 'flex-end',
+					my: 2
+				}}>
+					<Button variant='outlined' onClick={handleFormReset} >Clear</Button>
+					<Button variant='contained' type='submit'>Login</Button>
+				</Box>
 			</form>
 
 			{error && <p>{error.message}</p>}
