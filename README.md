d1e4daa5b145802041d2072403e9dae14acdeba0
Riajul Islam <javascriptforeverything@gmail.com>
2022-11-05 19:28:15 +0600

    refactoring seeder script

--- README.md
+++ README.md
@@ -1,9 +1,55 @@
 ## Getting Started
+	. Next.js
+	. TypeScript
+	. GraphQL API
+
+
+### Key Features
+	. Seeder Script: Import and delete data from database
+	. Dynamic form with proper types
+	. GraphQL (Server + Client) instead of **REST API**
+	. Material-UI: For front-end Designing
+
 
 First, run the development server:
 
 ```bash
-npm run dev
-# or
-yarn dev
+$ yarn dev
+```
+
+### Seeder Script
+Learn about seeder by `/data/server/seeder.ts`
+
+#### Seeder Commands
+```
+$ yarn seeder 				=> Read all 3 collections (users, products, reviews)
+$ yarn seeder --users 			=>   " 	Only 	users 	collection
+$ yarn seeder --products 		=>   " 	 "  	products   "
+$ yarn seeder --reviews 		=>   " 	 "  	reviews    "
+
+$ yarn seeder --import         		=> Import All 3 	collections	
+$ yarn seeder --import users 		=>   " 	   " 	users  	  "
+$ yarn seeder --import products 	=>   " 	   " 	products  "
+$ yarn seeder --import reviews 		=>   " 	   " 	reviews   "
+```
+
+
+#### Seeder Script in `package.json`
+```
+"scripts": {
+"dev": "next dev",
+"build": "next build",
+"start": "next start",
+"lint": "next lint",
+
+	"seeder" 		: "nodemon data/server/seeder.ts",
+	"seeder-users" 		: "nodemon data/server/seeder.ts --users",
+	"seeder-products" 	: "nodemon data/server/seeder.ts --products",
+	"seeder-reviews" 	: "nodemon data/server/seeder.ts --reviews",
+
+	"seeder-import" 	: "nodemon data/server/seeder.ts --import",
+	"seeder-import-users" 	: "nodemon data/server/seeder.ts --import users",
+	"seeder-import-products": "nodemon data/server/seeder.ts --import products",
+	"seeder-import-reviews" : "nodemon data/server/seeder.ts --import reviews"
+},
 ```
--- data/server/seeder.ts
+++ data/server/seeder.ts
@@ -1,6 +1,5 @@
 import dotEnv from 'dotenv'
-dotEnv.config({ path: '.env.local'})
-// must import very beging...
+dotEnv.config({ path: '.env.local'}) 	// must import very beging before dbConnect
 
 import { dbConnect } from '../../server/models/database'
 
@@ -21,13 +20,59 @@ const InsertHandler = async <T>(name: string, Model: any, data: T[]) => {
 	console.log(`---------[ importing ${name} success ]----------`) 
 }
 
-const getUsers = async () => {
-	const users = await User.find()
+const readDocs = async (Model: any, name='users') => {
+	const users = await Model.find()
 	console.log({ users })
+	console.log(`---------[ reading ${name} success ]----------`) 
 }
 
-InsertHandler<UserDocument>('users', User, users)
-InsertHandler<ProductDocument>('products', Product, products)
-InsertHandler<ReviewDocument>('reviews', Review, reviews)
-// getUsers()
+// InsertHandler<UserDocument>('users', User, users)
+// InsertHandler<ProductDocument>('products', Product, products)
+// InsertHandler<ReviewDocument>('reviews', Review, reviews)
+
+
+
+export {}
+
+const [,, operation, collection ] = process.argv
+// console.log({ operation, collection })
+
+
+if(operation === '--import') {
+	if(collection === 'users') 		InsertHandler<UserDocument>('users', User, users)
+	if(collection === 'products') InsertHandler<ProductDocument>('products', Product, products)
+	if(collection === 'reviews') 	InsertHandler<ReviewDocument>('reviews', Review, reviews)
+
+	if(!collection) {
+		InsertHandler<UserDocument>('users', User, users)
+		InsertHandler<ProductDocument>('products', Product, products)
+		InsertHandler<ReviewDocument>('reviews', Review, reviews)
+	}
+
+} else {
+	if(operation === '--users') 		readDocs(User, 'users')
+	if(operation === '--products') 	readDocs(Product, 'products')
+	if(operation === '--reviews') 	readDocs(Review, 'reviews')
+
+	if( operation !== '--users' && operation !== '--products' && operation !== '--reviews') {
+		readDocs(User, 'users')
+		readDocs(Product, 'producs')
+		readDocs(Review, 'reviews')
+	}
+}
+
+
+/* 
+		$ yarn seeder 										=> Read all 3 collections (users, products, reviews)
+		$ yarn seeder --users 						=> 	" 	Only users collection
+		$ yarn seeder --products 					=> 	" 		"  products 	"
+		$ yarn seeder --reviews 					=>  " 		"  reviews  	"
+
+		$ yarn seeder --import         		=> Import All 3 collections	
+		$ yarn seeder --import users 			=> 		" 	" 	users  		"
+		$ yarn seeder --import products 	=> 		" 	" 	products  "
+		$ yarn seeder --import reviews 		=> 		" 	" 	reviews  	"
+
+*/
+
 
