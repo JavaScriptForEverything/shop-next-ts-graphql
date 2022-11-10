a89433112936a5dcd4d7b01a3a226a9f08ccd40a
Riajul Islam <javascriptforeverything@gmail.com>
2022-11-10 19:18:58 +0600

    complete the /about page

---
+++ data/client/me.tsx
@@ -0,0 +1,88 @@
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import FacebookIcon from '@mui/icons-material/FacebookRounded'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import YoutubeIcon from '@mui/icons-material/YouTube'
+import { features } from 'process'
+
+
+
+export const socialMediaItems = [
+	{ label: 'github',   	icon: <GitHubIcon />, 	path: 'https://github.com/JavaScriptForEverything' },
+	{ label: 'linkedin', 	icon: <LinkedInIcon />, path: 'https://www.linkedin.com/in/JavaScriptForEverything/' },
+	{ label: 'twitter',  	icon: <TwitterIcon />, 	path: 'https://twitter.com/JSforEverything' },
+	{ label: 'facebook', 	icon: <FacebookIcon />, path: 'https://www.facebook.com/JavaScriptForEverything' },
+	{ label: 'instagram', icon: <InstagramIcon />, path: 'https://www.instagram.com/JavaScriptForEverything/' },
+	{ label: 'youtube',  	icon: <YoutubeIcon />, 	path: 'https://www.youtube.com/@JavaScriptForEverything/' },
+]
+
+export const webTechnologies = [
+  {
+    title: 'Next.js',
+    description: 'A Javascript Framework based on React library, to build production ready application with Server-Side rendering features.'
+  },
+  {
+    title: 'TypeScript',
+    description: 'Super set of JavaScript, it helps to create large application with minimum bugs'
+  },
+  {
+    title: 'GraphQL',
+    description: 'A alternative of RESTfull API, which overcome the limitation of REST API'
+  },
+  {
+    title: 'Redux',
+    description: 'To manage State of Client-Side application (React|Angular|View)'
+  },
+  {
+    title: 'Material-UI',
+    description: 'A CSS Framework to design application'
+  },
+  {
+    title: 'MongoDB(mongoose)',
+    description: 'Schema based Database Driver, to store data into MongoDB Database.'
+  },
+]
+
+export const mobileTechnologies = [
+  {
+    title: 'React-Native',
+    description: 'To create Cross-platform mobile application for android and ios'
+  },
+  {
+    title: 'Redux',
+    description: 'To manage State of Client-Side application (React|Angular|View)'
+  },
+  {
+    title: 'React-Native-Paper',
+    description: 'A CSS Framework to design application'
+  },
+  {
+    title: 'MongoDB(mongoose)',
+    description: 'Schema based Database Driver, to store data into MongoDB Database.'
+  },
+]
+
+
+export const developmentTools = [
+  {
+    title: 'Git & GitHub',
+    description: 'Version Controll System. Used to manage and share project'
+  },
+  {
+    title: 'Docker & DockerHub',
+    description: 'To test application to any machine and also deploy project'
+  },
+  {
+    title: 'Linux OS',
+    description: 'Linux OS: similer as English Language, no mater where you go, you have to know a little bit.'
+  },
+  {
+    title: 'SSH',
+    description: 'To communicate with any Computer through internet, specially with Server'
+  },
+  {
+    title: 'VirtualBox',
+    description: 'Vittualization is a technologies, which give you virtual mechine to test and also in production'
+  }
+]
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,101 @@
+import Image from 'next/image'
+
+import * as myData from '@/data/client/me'
+
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import Avatar  from '@mui/material/Avatar'
+import IconButton  from '@mui/material/IconButton'
+import Tooltip  from '@mui/material/Tooltip'
+import Divider  from '@mui/material/Divider'
+import MuiLink  from '@mui/material/Link'
+
+type ArrayObject = {
+	title: string
+	description: string
+}
+
+const getStack = <T extends ArrayObject>(title: string, stackArrayObject: T[], divider=true) => (
+	<Box>
+		<Typography component='h1' variant='h6' color='primary' sx={{ textTransform: 'uppercase'}}> {title} </Typography>
+		<Box sx={{ mt: 2, px: 2 }}>
+			{stackArrayObject.map(({ title, description}) => (
+				<Box key={title} sx={{ mb: 2 }}>
+					<Typography variant='body1'> {title} </Typography>
+					<Typography variant='body2' color='textSecondary'> {description} </Typography>
+				</Box>
+			))}
+		</Box>
+		{divider && <Divider sx={{ my: 2 }} /> }
+	</Box>
+)
+
 
 const About = () => {
 
 	return (
 		<>
-			<p>About Page</p>
+			<Grid container spacing={2} sx={{ my: 2 }}>
+				{/*-----[ Left Side ]------*/}
+				<Grid item xs={12} md={4}>
+					<Paper  sx={{
+						py: 1, px: 2,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						gap: 1
+					}}>
+
+						<Avatar 
+							sx={{
+								width: 200, height: 200,
+								border: '0.1px solid lightgrey'
+							}} 
+						>
+							<Image src='/images/aboutAvatar.png' alt='aboutAvatar' layout='fill'/>
+						</Avatar>
+						<Typography color='primary' sx={{ mt: 1 }}>Riajul Islam</Typography>
+						<Typography>FullStack Developer: Next.js, TypeScript, GraphQL</Typography>
+
+						<Box>
+						{ myData.socialMediaItems.map(({ label, icon, path }) => (
+							<IconButton key={label} color='inherit'>
+								<Tooltip title={label}>
+									<MuiLink href={path} target='_blank' color='inherit'> {icon} </MuiLink>
+								</Tooltip>
+							</IconButton>
+						))}
+						</Box>
+
+						<Typography variant='body2' color='textSecondary' paragraph>
+							Hi there, I am a <em>JavaScript Programmer</em>.  My Primary Goal is to building Web Application by <em><strong>Next.js</strong></em>, <em><strong>TypeScript</strong></em>, <em><strong>GraphQL</strong></em>, <em>React</em>, <em>Redux</em>, <em>Material-UI</em> in front-end and <em>Node</em>& <em>MongoDB(mongoose)</em> in back-end.
+						</Typography>
+						<Typography variant='body2' color='textSecondary' paragraph>
+							I&apos;m a <em><strong>self-taught programmer</strong></em> but I&apos;m trying to fill-up that gap by learning everyday something new on developing topic.  
+						</Typography>
+
+						<Typography variant='h6' component='blockquote' color='textSecondary'>
+							&ldquo; The more I learned the more I wondered.  &rdquo;
+						</Typography>
+					</Paper>
+				</Grid>
+
+				{/*-----[ Right Side ]------*/}
+				<Grid item xs={12} md={8}>
+					<Paper sx={{ py: 1, px: 2 }}>
+
+					{ getStack('Web Development Stack', myData.webTechnologies) }
+					{ getStack('Mobile App Development Stack', myData.mobileTechnologies) }
+					{ getStack('Development Tools', myData.developmentTools, false) }
+
+					</Paper>
+				</Grid>
+			</Grid>
 		</>
 	)
 }
 export default About
+
+
