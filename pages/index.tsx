23824d3df32e94568422cb9b86c2f120918f6b66
Riajul Islam <javascriptforeverything@gmail.com>
2022-12-24 17:54:28 +0600

    added { mb: 1 } in serarchBar in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,32 +25,31 @@ const Home = () => {
 	return (
 		<>
 
-		<Grid container sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+		<Grid container sx={{ display: 'flex', justifyContent: 'flex-end', mb: 1 }}>
 			<Grid item xs={12} sm={6} md={3}>
 				<SearchBar />
 			</Grid>
 		</Grid>
 
-			<Grid container spacing={2}>
-				<Grid item xs={12} md={3} sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					gap: 1
-				}}>
-					<Paper> <FilterRating /> </Paper>
-					<Paper> <FilterBrands /> </Paper>
-					<Paper> <FilterPrice /> </Paper>
-					<Paper> <FilterSize /> </Paper>
-				</Grid>
-
-				<Grid item xs={12} md={9}>
-					<TitleBar />
-					<Box sx={{ my: 1 }}>
-						<ProductContainer />
-					</Box>
-				</Grid>
+		<Grid container spacing={2}>
+			<Grid item xs={12} md={3} sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				gap: 1
+			}}>
+				<Paper> <FilterRating /> </Paper>
+				<Paper> <FilterBrands /> </Paper>
+				<Paper> <FilterPrice /> </Paper>
+				<Paper> <FilterSize /> </Paper>
 			</Grid>
 
+			<Grid item xs={12} md={9}>
+				<TitleBar />
+				<Box sx={{ my: 1 }}>
+					<ProductContainer />
+				</Box>
+			</Grid>
+		</Grid>
 
 		</>
 	)
